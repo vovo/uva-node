1fdb520fc7f09c60da3c163c8e1b251563705284
Lucas Tan <tbhtan3@gmail.com>
2013-04-20 14:50:25 +0800

    added features
    - able to select number of most recent subs
    - able to see submission times

--- index.js
+++ index.js
@@ -47,25 +47,37 @@ rl.on('line', function(line) {
 
     function printStatus(subs)
     {
-        console.log("SubId     | Prob # |      Verdict     |  Lang  | Runtime | ");
-        //           123456789---123456---1234567890123456---123456---1234567---
+        console.log("Sub Id    | Prob # |      Verdict     |  Lang  | Runtime |  Rank |      Sub Time");
+        //           123456789---123456---1234567890123456---123456---1234567---12345---yyyy-mm-dd hh:mm:ss
 
-        var iterLen = Math.min(10, subs.length);
-        for (var i = 0; i < iterLen;i++)
+        var date = new Date();
+        for (var i = 0; i < subs.length;i++)
         {
             var sub = subs[i];
             var subId = sub[0];
             var probId = sub[1];
             var verdict = sub[2];
             var runtime = sub[3];
-            var time = sub[4];
+            var time = sub[4]; // in millisec
             var lang = sub[5];
             var rank = sub[6];
 
-            console.log(sprintf("%9d   %6d   %16s   %6s   %3d.%03d", 
+            date.setTime(time);
+            console.log(sprintf("%9d   %6d   %16s   %6s   %3d.%03d   %5s   %4d-%02d-%02d %02d:%02d:%02d", 
                 subId, probId, verdict,
-                lang, Math.floor(runtime/1000), runtime%1000));
+                lang, Math.floor(runtime/1000), runtime%1000,
+                rank < 0 ? '-' : rank > 9999 ? '>9999' : rank,
+                date.getFullYear(), date.getMonth()+1, date.getDate(),
+                date.getHours(), date.getMinutes(), date.getSeconds()));
         }
+    }
+
+    function getCurrentAdapter()
+    { 
+        var curAdap = app.getCurrentAdapter();
+        if (curAdap) return curAdap;
+
+        console.log('No current account selected');
     }
 
     switch(action) 
@@ -77,20 +89,24 @@ rl.on('line', function(line) {
         break;
 
     case 'send':
+        var curAdap = getCurrentAdapter();
+        if (!curAdap) break;
+        
         if (!checkToks(2, 'send <prob#> <fileName>')) break;
 
         try
         {
             console.log('Logging in...');
-            var ok = app.login(function(e){
+            curAdap.login(function(e){
                 if (e)
                 {
                     console.log('Login error: '+e.message);
-                    return prompt();
+                    prompt();
+                    return;
                 }
 
                 console.log('Sending code...');
-                app.send(toks[1], toks[2], function(e){
+                curAdap.send(toks[1], toks[2], function(e){
                     if (e)
                         console.log('send failed: '+e.message);
                     else
@@ -99,9 +115,7 @@ rl.on('line', function(line) {
                 });    
             });
 
-            if (ok) return;
-            
-            console.log('No current account selected');
+            return;
         }
         catch (e)
         {
@@ -181,8 +195,27 @@ rl.on('line', function(line) {
 
     case 'stat':
     case 'status':
+        var curAdap = getCurrentAdapter();
+        if (!curAdap) break;
+        
+        var num = 10;
+        if (toks.length == 2) 
+        {
+            num = parseInt(toks[1]);
+            if (num <= 0 || isNaN(num))
+            {
+                console.log('must be positive integer');
+                break;
+            }
+        }
+        else if (toks.length != 1)
+        {
+            console.log('Syntax: stat/status <count>');
+            break;
+        }
+
         console.log('Getting status...');
-        var ok = app.fetchStatus(function(e, subs){
+        curAdap.fetchStatus(num, function(e, subs){
             if (e)
                 console.log('Status error: '+e.message);
             else
@@ -190,10 +223,7 @@ rl.on('line', function(line) {
             prompt();
         });
 
-        if (ok) return;
-
-        console.log('No current account selected');
-        break;
+        return;
 
     default:
         console.log('Unrecognized action');
